9ae5d9eda0c12ba26b6071d2f50834a577be757e
Кудрявцев Дмитрий <dvkudryavt@mts.ru>
2022-10-26 17:16:00 +0300

    move label from form to child template

--- src/app/pages/inventory/parameter-selector/parameter-selector/parameter-selector.component.ts
+++ src/app/pages/inventory/parameter-selector/parameter-selector/parameter-selector.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-parameter-selector',
   templateUrl: './parameter-selector.component.html',
   styleUrls: ['./parameter-selector.component.scss'],
 })
-export class ParameterSelectorComponent {}
+export class ParameterSelectorComponent {
+  @Input() labelInputId = '';
+  @Input() labelText = '';
+}
 
