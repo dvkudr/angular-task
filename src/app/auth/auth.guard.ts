c86bd3d8edb9cb5e6d51db2941aa1d0f8bf69327
Кудрявцев Дмитрий <dvkudryavt@mts.ru>
2022-10-26 16:44:55 +0300

    code cleanup

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,13 +8,13 @@ import { AuthService } from './auth.service';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): Observable<boolean> {
+  canActivate(): boolean {
     if (this.authService.isAuthenticated()) {
-      return of(true);
+      return true;
     }
 
     this.router.navigate(['/login']);
-    return of(false);
+    return false;
   }
 }
 
