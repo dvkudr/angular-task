040ec6fdac8871c2e0ae966dcf5f7d319c701ac6
Кудрявцев Дмитрий <dvkudryavt@mts.ru>
2022-10-26 18:55:17 +0300

    code cleanup

--- src/app/interceptors/username.interceptor.ts
+++ src/app/interceptors/username.interceptor.ts
@@ -6,24 +6,19 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserNameInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
-
   intercept(
     req: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    if (this.authService.isAuthenticated()) {
-      req = req.clone({
-        setHeaders: {
-          'x-user-name': environment.userName,
-        },
-      });
-    }
+    req = req.clone({
+      setHeaders: {
+        'x-user-name': environment.userName,
+      },
+    });
 
     return next.handle(req);
   }
