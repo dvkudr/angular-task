9bd55ec81830bac401bd3523a7da8d59684dc703
Кудрявцев Дмитрий <dvkudryavt@mts.ru>
2022-11-01 18:58:08 +0300

    some more ngrx code

--- src/app/store/common/effects/common.effects.ts
+++ src/app/store/common/effects/common.effects.ts
@@ -1,10 +1,30 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Router } from "@angular/router";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { catchError, map, of, switchMap } from "rxjs";
+import { AuthToken, CommonService } from "src/app/shared/services/common.service";
+import { CommonActions } from "../actions/common.action";
 
 @Injectable()
 export class CommonEffects {
     constructor(
-        private actions$: Actions
-
+        private actions$: Actions,
+        private router: Router,
+        private commonService: CommonService
     ) { }
+
+    fetchToken$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(CommonActions.getToken),
+            switchMap(({ payload }) => this.commonService.fetchToken(payload.login, payload.password)),
+            map((result: AuthToken) => {
+                localStorage.setItem('token', result.access_token);
+
+                this.router.navigateByUrl('/inventory');
+
+                return CommonActions.getTokenSuccess({ payload: { token: result.access_token } });
+            }),
+            catchError((error) => of(CommonActions.getTokenError({ payload: { error: error.message } })))
+        )
+    })
 }
